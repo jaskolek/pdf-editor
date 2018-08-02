262196229f8de8dc2ff53cba7db57db3b0bb3f5f
jaskolek <jaskolek@gmail.com>
2018-08-02 18:09:34 +0200

    Update compressor to use ~10 times less memory

--- src/PdfEditor/PdfImageResizer/DeviceCMYK8PdfImageResizer.php
+++ src/PdfEditor/PdfImageResizer/DeviceCMYK8PdfImageResizer.php
@@ -40,55 +40,103 @@ class DeviceCMYK8PdfImageResizer implements PdfImageResizerInterface
      */
     public function resize($source, $width, $height, $newWidth, $newHeight): string
     {
-        $channelList = $this->getChannelList($source);
+        $length = \strlen($source) / 4;
 
-        $resizedChannelImageList = [];
-        foreach ($channelList as $channelIndex => $channel) {
+        $outputCharacterList = [];
+        for ($channelIndex = 0; $channelIndex < 4; ++$channelIndex) {
             $channelImage = imagecreatetruecolor($width, $height);
-            foreach ($channel as $index => $pixel) {
-                $color = imagecolorallocate($channelImage, $pixel, $pixel, $pixel);
-                $x = $index % $width;
-                $y = floor($index / $width);
+
+            for ($pixelIndex = 0; $pixelIndex < $length; ++$pixelIndex) {
+                $pixelValue = \ord($source[$pixelIndex * 4 + $channelIndex]);
+                $color = imagecolorallocate($channelImage, $pixelValue, $pixelValue, $pixelValue);
+                $x = $pixelIndex % $width;
+                $y = floor($pixelIndex / $width);
+
                 imagesetpixel($channelImage, $x, $y, $color);
             }
 
             $resizedChannelImage = imagecreatetruecolor($newWidth, $newHeight);
             imagecopyresampled($resizedChannelImage, $channelImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-            $resizedChannelImageList[] = $resizedChannelImage;
-        }
+            imagedestroy($channelImage);
 
-
-        $output = '';
-        for ($y = 0; $y < $newHeight; ++$y) {
-            for ($x = 0; $x < $newWidth; ++$x) {
-                foreach ($resizedChannelImageList as $resizedChannelImage) {
+            for ($y = 0; $y < $newHeight; ++$y) {
+                for ($x = 0; $x < $newWidth; ++$x) {
+                    $pixelIndex = ($x + $y * $newWidth) * 4 + $channelIndex;
                     $imageColor = imagecolorat($resizedChannelImage, $x, $y);
-                    $value = imagecolorsforindex($resizedChannelImage, $imageColor);
-                    $output .= \chr(floor($value['red'] / $this->channelColorReductionLevel) * $this->channelColorReductionLevel);
-//                    $output .= \chr($value['red']);
+                    $colorValue = imagecolorsforindex($resizedChannelImage, $imageColor);
+                    $pixelValue = \chr(floor($colorValue['red'] / $this->channelColorReductionLevel) * $this->channelColorReductionLevel);
+//                    $pixelValue = \chr($colorValue['red']);
+
+                    $outputCharacterList[$pixelIndex] = $pixelValue;
                 }
             }
+            imagedestroy($resizedChannelImage);
         }
-        return $output;
+        ksort($outputCharacterList);
+        return implode('', $outputCharacterList);
     }
 
 
-    /**
-     * @param $source
-     * @return array[]
-     */
-    protected function getChannelList($source): array
-    {
-        $channelList = [[], [], [], []];
-        $pixelStringList = str_split($source, 4);
-        foreach ($pixelStringList as $pixelString) {
-            $channelList[0][] = \ord($pixelString[0]);
-            $channelList[1][] = \ord($pixelString[1]);
-            $channelList[2][] = \ord($pixelString[2]);
-            $channelList[3][] = \ord($pixelString[3]);
-        }
-
-        return $channelList;
-    }
+//
+//    /**
+//     * @param $source
+//     * @param $width
+//     * @param $height
+//     * @param $newWidth
+//     * @param $newHeight
+//     * @return string
+//     */
+//    public function resize($source, $width, $height, $newWidth, $newHeight): string
+//    {
+//        $channelList = $this->getChannelList($source);
+//
+//        $resizedChannelImageList = [];
+//        foreach ($channelList as $channelIndex => $channel) {
+//            $channelImage = imagecreatetruecolor($width, $height);
+//            foreach ($channel as $index => $pixel) {
+//                $color = imagecolorallocate($channelImage, $pixel, $pixel, $pixel);
+//                $x = $index % $width;
+//                $y = floor($index / $width);
+//                imagesetpixel($channelImage, $x, $y, $color);
+//            }
+//
+//            $resizedChannelImage = imagecreatetruecolor($newWidth, $newHeight);
+//            imagecopyresampled($resizedChannelImage, $channelImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+//            $resizedChannelImageList[] = $resizedChannelImage;
+//        }
+//
+//
+//        $output = '';
+//        for ($y = 0; $y < $newHeight; ++$y) {
+//            for ($x = 0; $x < $newWidth; ++$x) {
+//                foreach ($resizedChannelImageList as $resizedChannelImage) {
+//                    $imageColor = imagecolorat($resizedChannelImage, $x, $y);
+//                    $value = imagecolorsforindex($resizedChannelImage, $imageColor);
+//                    $output .= \chr(floor($value['red'] / $this->channelColorReductionLevel) * $this->channelColorReductionLevel);
+////                    $output .= \chr($value['red']);
+//                }
+//            }
+//        }
+//        return $output;
+//    }
+//
+//
+//    /**
+//     * @param $source
+//     * @return array[]
+//     */
+//    protected function getChannelList($source): array
+//    {
+//        $channelList = [[], [], [], []];
+//        $pixelStringList = str_split($source, 4);
+//        foreach ($pixelStringList as $pixelString) {
+//            $channelList[0][] = \ord($pixelString[0]);
+//            $channelList[1][] = \ord($pixelString[1]);
+//            $channelList[2][] = \ord($pixelString[2]);
+//            $channelList[3][] = \ord($pixelString[3]);
+//        }
+//
+//        return $channelList;
+//    }
 
 }
